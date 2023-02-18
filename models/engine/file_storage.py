62480dbffc03380979372c8fa812e514f2c59d64
iman254 <stephaniemarquinz@icloud.com>
2023-02-18 23:14:21 +0200

    task

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,7 @@ module that serializes instances to a JSON file
 and deserializes JSON file to instances
 """
 
+
 import json
 from models.base_model import BaseModel
 from models.user import User
@@ -12,6 +13,7 @@ from models.city import City
 from models.state import State
 from models.amenity import Amenity
 from models.review import Review
+
 
 class FileStorage:
     """
